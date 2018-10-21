ba1e61782a8ffb641a12b53bed6d78d5e66b6b22
unknown <chuangmatthew@yahoo.com>
2018-10-21 16:41:21 -0400

    basic components for each page set up. Need to do routing now

---
+++ my-app/src/Calendar.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+ 
+class Calendar extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Calendar</h2>
+
+        <p>A really sick calendar goes here yo.</p>
+      </div>
+    );
+  }
+}
+ 
+export default Calendar;
