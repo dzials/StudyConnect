2d0870ba871c23d4b21285ff0729f7a5ad2d7bdd
unknown <chuangmatthew@yahoo.com>
2018-10-28 21:36:40 -0400

    Login form created. Need CSS styling, and account creation form. Form currently does not take state from redux and is not linked to back end yet (form is inert)

--- client/src/Login/Login.js
+++ client/src/Login/Login.js
@@ -1,10 +1,64 @@
 import React, { Component } from 'react';
+import { Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
+//TODO: CSS styling
 
+//TODO: Have the component source from Redux
+//TODO: Create account form in state
 export default class Login extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: "",
+      password: ""
+    };
+  }
+
+  formIsValid() {
+    return this.state.email.length > 0 && this.state.password.length > 0;
+  }
+
+  updateInfo = event => {
+    this.setState({
+      [event.target.id]: event.target.value
+    });
+  }
+
+  submitForm = event => {
+    //TODO: Log-in through backend
+  }
+
   render() {
     return (
-      <div className="container">
-        We will put a login form here!
+      <div className="Login">
+        <form onSubmit={this.submitForm}>
+          <FormGroup ControlId="email" bsSize="Large">
+            <ControlLabel>Email</ControlLabel>
+            <FormControl
+              autoFocus
+              type="email"
+              value={this.state.email}
+              onChange={this.updateInfo}
+            />
+          </FormGroup>
+          <FormGroup ControlId="password" bsSize="Large">
+            <ControlLabel>Password</ControlLabel>
+            <FormControl
+              value={this.state.password}
+              onChange={this.handleChange}
+              type="password"
+            />
+          </FormGroup>
+          <Button
+            block
+            bsSize="Large"
+            disabled={!this.formIsValid()}
+            type="submit"
+          >
+            Login
+          </Button>
+        </form>
       </div>
     )
   }
