6375d164669c580ebaf4ee84a6106694dfd8371e
right3ous <will.eiref@gmail.com>
2018-11-04 20:05:48 -0500

    nodeJS not accepting calendar

--- client/src/Calendar/Calendar.js
+++ client/src/Calendar/Calendar.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
+import $ from 'jquery';
+import 'fullcalendar';
+
 
 // TODO - have this component source from redux
+$(function() {
+
+	// page is now ready, initialize the calendar...
+
+	$('#calendar').fullCalendar({
+
+		// weekends: false // will hide Saturdays and Sundays
+	})
+});
+
+
 export default class Calendar extends Component {
+
   render() {
     return (
-      <div className="container">
-        We will put a calendar on this page!
-      </div>
+		<div id='calendar'></div>
     )
   }
 }
